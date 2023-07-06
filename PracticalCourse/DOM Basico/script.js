36b3c5e6e3e44d627ee8226b7fdde5c50d126f97
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-07-05 21:58:01 -0500

    Proving JS in HTML
    My first HTML file which integrates JS script was created in the project

---
+++ PracticalCourse/DOM Basico/script.js
@@ -0,0 +1,2 @@
+console.log("This is a log");
+console.log(document.getElementById('title').innerText);
--- proofs.js
+++ proofs.js
@@ -1,1 +1,10 @@
-console.log(typeof "Proofs.js loaded")
+let number = 1;
+let stringN = "1";
+
+if (number > 0) {
+    let stringN = "2";
+    console.log(stringN);
+}
+
+console.log(stringN);
+console.warn("This is a warning");
